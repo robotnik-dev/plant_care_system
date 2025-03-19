a1fab4b38476badede1927b8c10121fa69930e0d
robotnik-dev <robotnik.games@gmail.com>
2025-03-19 19:30:22 +0100

    feat: add cluster IP

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -27,7 +27,7 @@ fn app() -> Html {
         use_effect_with((), move |_| {
             let readings = readings.clone();
             wasm_bindgen_futures::spawn_local(async move {
-                let fetched_readings = Request::get("/api/v1/readings")
+                let fetched_readings = Request::get("http://10.43.20.48/api/v1/readings")
                     .send()
                     .await
                     .unwrap()
